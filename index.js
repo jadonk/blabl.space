b012605805f39618673817f2056bbe33afb63058
Jason Kridner <jkridner@beagleboard.org>
2021-12-31 20:01:41 +0000

    WIP

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
   );
 });
 
+const codes = {
+  "code-1": "asdf",
+  "code-2": "qwerty",
+};
+
 app.use(express.static('files'));
 app.use(express.static('node_modules/bootstrap/dist'));
 app.use(express.static('node_modules/jquery/dist'));
@@ -17,7 +22,8 @@ app.use(express.static('node_modules/jquery/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.post("/post", (req, res) => {
-  console.log("post: %j",  req.body);
+  req.body.unlocked = (codes[req.body.lock] == req.body.code);
+  console.log("code: %s, post: %j",  codes[req.body.lock], req.body);
   res.json(req.body);
 });
 
